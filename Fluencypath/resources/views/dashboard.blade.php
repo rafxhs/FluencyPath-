6b9d3624a27b00efeb4a57678de4a40e0613d9ad
Erick Souza <erickdasilvasouza824@gmail.com>
2025-01-23 12:18:22 -0300

    corrigindo bug de perfil

--- Fluencypath/resources/views/dashboard.blade.php
+++ Fluencypath/resources/views/dashboard.blade.php
@@ -1,5 +1,8 @@
-<x-app-layout>
-    <x-slot name="header">
+
+@extends('layouts.app')
+
+@section('content')
+<x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
@@ -14,4 +17,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
--- Fluencypath/resources/views/profile/edit.blade.php
+++ Fluencypath/resources/views/profile/edit.blade.php
@@ -1,5 +1,8 @@
-<x-app-layout>
-    <x-slot name="header">
+@extends('layouts.app')
+
+@section('content')
+
+<x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
@@ -26,4 +29,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
+
