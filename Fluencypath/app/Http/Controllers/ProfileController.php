1ec2cf34050b09ebe8ac25df44ae622912b01ded
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-01-29 11:05:21 -0300

    acrescentação da função show no controller de profile

--- Fluencypath/app/Http/Controllers/ProfileController.php
+++ Fluencypath/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -35,6 +36,12 @@ class ProfileController extends Controller
         $request->user()->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('profile.show', compact('user'));
     }
 
     /**
