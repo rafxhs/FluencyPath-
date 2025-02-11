3590ee3da3ce12dfa8bb8427c26efb108c0f5dbf
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-02-11 08:54:14 -0300

    style: modificação no layout dos componentes

--- Fluencypath/resources/views/components/text-input.blade.php
+++ Fluencypath/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-neutral-300 font-primary font-medium text-sm focus:border-primary-700 focus:ring-primary-700 rounded-lg shadow-sm']) }}>
