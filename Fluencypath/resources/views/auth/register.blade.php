35dd04bcf865bf739c7298db6c3f2f63e96a59ee
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-02-11 08:14:53 -0300

    style: frontend das páginas de gerenciamento de senha

--- Fluencypath/resources/views/auth/register.blade.php
+++ Fluencypath/resources/views/auth/register.blade.php
@@ -1,25 +1,35 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.register')
 
-        <div class="text-2xl card-header">{{ __('Cadastre-se') }}</div>
+@section('content')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Nome')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" placeholder="Nome" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+<div class="mb-10 font-primary font-bold text-2xl text-neutral-600 card-header">
+    <h1>FluencyPath</h1>
+</div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" placeholder="Email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<form method="POST" action="{{ route('register') }}">
+    @csrf
 
+    <div class="mb-10 font-primary font-medium text-2xl text-neutral-600">{{ __('Cadastre-se') }}</div>
+
+    <!-- Name -->
+    <div>
+        <x-input-label for="name" :value="__('Nome')" />
+        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" placeholder="Nome" :value="old('name')" required autofocus autocomplete="name" />
+        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    </div>
+
+    <!-- Email Address -->
+    <div class="mt-5">
+        <x-input-label for="email" :value="__('Email')" />
+        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" placeholder="exemplo@email.com" :value="old('email')" required autocomplete="username" />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+    <!-- Password container -->
+
+    <div class="flex justify-between gap-5 mt-5">
         <!-- Password -->
-        <div class="mt-4">
+        <div class="w-full">
             <x-input-label for="password" :value="__('Senha')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -32,7 +42,7 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        <div class="w-full">
             <x-input-label for="password_confirmation" :value="__('Confirmar senha')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
@@ -41,25 +51,37 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button class="ms-4">
-                {{ __('Cadastre-se') }}
-            </x-primary-button>
-
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Já possui conta? Entrar') }}
-            </a>
-
-        </div>
-
-    </form>
-    <div class="mt-6 space-y-4">
-        <a href=" {{ route('redirect.google') }} ">
-            <button class="w-full flex items-center justify-center border-2 border-gray-300 text-gray-600 py-2 rounded-md shadow-md hover:bg-gray-50 transition duration-200">
-
-                <img src="https://cdn-icons-png.flaticon.com/256/2875/2875404.png" alt="Google logo" class="w-5 h-auto"> Continuar com o Google
-            </button>
-        </a>
     </div>
-</x-guest-layout>
+
+    <div class="flex items-center justify-end mt-10">
+        <x-primary-button>
+            {{ __('Cadastre-se') }}
+        </x-primary-button>
+    </div>
+
+    <div class="flex items-center justify-end mt-5 gap-2">
+        <span class="font-primary font-medium text-sm text-neutral-500">Já tem uma conta?</span>
+        <a class="font-primary font-medium text-sm text-primary-700 hover:text-primary-700 hover:underline rounded-md" href="{{ route('login') }}">
+            {{ __('Entrar') }}
+        </a>
+
+    </div>
+
+</form>
+
+<div class="flex items-center justify-center space-x-4 mt-8">
+    <hr class="flex-grow text-neutral-200 ">
+    <span class="font-secondary text-neutral-400 text-base">ou</span>
+    <hr class="flex-grow text-neutral-200">
+</div>
+
+<div class="mt-8 space-y-4">
+    <a href=" {{ route('redirect.google') }} ">
+        <button class="w-full h-[45px] flex flex-row gap-4 items-center justify-center border border-primary-700 font-primary font-medium text-sm text-primary-1000 py-2 rounded-md shadow-md hover:border-primary-400 hover:border-1.5 focus:border-primary-700 focus:border-2 transition duration-200">
+
+            <img src="https://cdn-icons-png.flaticon.com/256/2875/2875404.png" alt="Google logo" class="w-5 h-auto">
+            <span>Continuar com o Google</span>
+        </button>
+    </a>
+</div>
+@endsection
