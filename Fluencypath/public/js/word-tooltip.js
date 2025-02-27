80d7da0939b4cf608ab1deff08c70fbb3db02770
Erick Souza <erickdasilvasouza824@gmail.com>
2025-02-27 14:13:49 -0300

    correrção ortográfica

--- Fluencypath/public/js/word-tooltip.js
+++ Fluencypath/public/js/word-tooltip.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
         word.addEventListener("click", async function (event) {
             let wordText = this.getAttribute("data-word");
             let tooltip = document.getElementById("tooltip");
-            console.log(`Buscando palavra: ${wordText}`); 
+            console.log(`Buscando palavra: ${wordText}`);
 
             try {
                 let response = await fetch(`/word/${wordText}`);
                 let data = await response.json();
 
                 if (data.error) {
-                    tooltip.innerHTML = "Esta palavra ainda não esta diponivel!  &#128517;";
+                    tooltip.innerHTML = "Esta palavra ainda não está disponível!  &#128517;";
                 } else {
                     let audioButton = data.audio
                         ? `<button onclick="new Audio('${data.audio}').play()">🔊 Ouvir</button>`
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     tooltip.innerHTML = `
                         <strong>${data.word}</strong> <br>
-                        Pronuncia: <em>${data.pronunciation || "Pronúncia não dísponivel."}</em> <br>
+                        Pronuncia: <em>${data.pronunciation || "Pronúncia não disponível."}</em> <br>
                         Tradução: ${data.translation}<br>
                         ${audioButton}
                     `;
                 }
 
                 tooltip.classList.remove("hidden");
-                
+
                 // Ajusta a posição do tooltip próximo à palavra clicada
                 let rect = this.getBoundingClientRect();
                 tooltip.style.left = `${rect.left + window.scrollX}px`;
