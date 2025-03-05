1e7e3935380b946b365e7189d720f7455348fb42
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-03-05 17:07:33 -0300

    style: reajustes no footer

--- Fluencypath/resources/views/layouts/footer.blade.php
+++ Fluencypath/resources/views/layouts/footer.blade.php
@@ -1,55 +1,57 @@
-<footer class="relative w-full bg-cyan-600 h-[400px]">
-  <div class="w-full px-8 mx-auto max-w-7xl">
-    <div class="grid justify-between grid-cols-1 gap-4 md:grid-cols-2">
+<footer class="flex items-center justify-center w-full bg-cyan-600 h-[400px]">
+  <div class="w-full mx-auto max-w-7xl">
+    <div class="flex justify-between">
       <h5 class="mb-6 text-xl font-semibold text-white">
         FluencyPath
       </h5>
-      <div class="grid justify-between grid-cols-3 gap-4">
-        <ul>
-          <p class="block mb-1 text-base font-semibold  text-white">
-            Funcionalidades
-          </p>
-          <li>
-            <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
-              Textos
-            </a>
-          </li>
-          <li>
-            <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
-              Flashcards
-            </a>
-          </li>
-        </ul>
-        <ul>
-          <p class="block mb-1 text-base font-semibold text-white">
-            Suporte
-          </p>
-          <li>
-            <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
-              Segurança
-            </a>
-          </li>
-          <li>
-            <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
-              Termos & Privacidade
-            </a>
-          </li>
-        </ul>
-        <ul>
-          <p class="block mb-1 text-base font-semibold text-white">
-            Empresa
-          </p>
-          <li>
-            <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
-              Sobre nós
-            </a>
-          </li>
-        </ul>
+      <div class="justify-between gap-4 md:grid-cols-2">
+        <div class="grid justify-between grid-cols-3 gap-10">
+          <ul>
+            <p class="block mb-1 text-base font-semibold  text-white">
+              Funcionalidades
+            </p>
+            <li>
+              <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
+                Textos
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
+                Flashcards
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <p class="block mb-1 text-base font-semibold text-white">
+              Suporte
+            </p>
+            <li>
+              <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
+                Segurança
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
+                Termos & Privacidade
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <p class="block mb-1 text-base font-semibold text-white">
+              Empresa
+            </p>
+            <li>
+              <a href="#" class="block text-white py-1 hover:text-slate-500 focus:text-slate-500 text-sm">
+                Sobre nós
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <div class="flex flex-col items-center justify-center w-full py-4 mt-12 border-t border-slate-200 md:flex-row md:justify-between">
       <p class="block mb-4 text-sm text-center text-white md:mb-0">
-        © {{ date('Y') }} 
+        © {{ date('Y') }}
         <a href="https://material-tailwind.com/">FluencyPath</a>. Todos
         os direitos reservados.
       </p>
