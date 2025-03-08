a0f525666a70f5911716a5b9773c8ab8406b295a
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-03-08 09:48:44 -0300

    style: add icones do heroicons

--- Fluencypath/resources/views/auth/reset-password.blade.php
+++ Fluencypath/resources/views/auth/reset-password.blade.php
@@ -4,9 +4,7 @@
 
         <div class="relative">
             <div class="absolute -top-16 left-1/2 -translate-x-1/2  bg-primary-700 rounded-full w-32 h-32 flex justify-center items-center shadow-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 text-primary-200">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-                </svg>
+                <x-heroicon-o-lock-closed class="w-16 h-16 text-primary-200" />
             </div>
         </div>
 
